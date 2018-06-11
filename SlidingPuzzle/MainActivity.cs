161a86636f6292055918b0ea87d45ea2bae68c12
Oxie <oxieking83@gmail.com>
2018-06-11 22:17:21 +0100

    can start app using emulator

--- SlidingPuzzle/MainActivity.cs
+++ SlidingPuzzle/MainActivity.cs
@@ -2,18 +2,41 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Graphics;
 
 namespace SlidingPuzzle
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
+        #region
+        Button resetButton;
+        GridLayout mainLayout;
+
+        int gameViewWidth;
+        #endregion
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            SetGameView();
+           }
+
+        private void SetGameView()
+        {
+            resetButton = FindViewById<Button>(Resource.Id.resetButtonId);
+            mainLayout = FindViewById<GridLayout>(Resource.Id.gameGridLayoutId);
+
+            gameViewWidth = Resources.DisplayMetrics.WidthPixels;
+            mainLayout.ColumnCount = 4;
+            mainLayout.RowCount = 4;
+
+            mainLayout.LayoutParameters = new RelativeLayout.LayoutParams(gameViewWidth, gameViewWidth);
+            mainLayout.SetBackgroundColor(Color.Gray);
         }
     }
 }
