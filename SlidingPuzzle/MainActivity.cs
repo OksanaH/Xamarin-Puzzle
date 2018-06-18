de559a3c0ac5595a7c7e98258e09b90832338bdf
Oxie <oxieking83@gmail.com>
2018-06-18 20:41:37 +0100

    Added method for sliding tiles

--- SlidingPuzzle/MainActivity.cs
+++ SlidingPuzzle/MainActivity.cs
@@ -20,6 +20,8 @@ namespace SlidingPuzzle
 
         int gameViewWidth;
         int tileWidth;
+
+        Point emptySpot;
         
         #endregion
 
@@ -48,6 +50,7 @@ namespace SlidingPuzzle
 
             mainLayout.LayoutParameters = new RelativeLayout.LayoutParams(gameViewWidth, gameViewWidth);
             mainLayout.SetBackgroundColor(Color.Gray);
+
         }
 
         private void MakeTiles()
@@ -60,7 +63,7 @@ namespace SlidingPuzzle
                
                 for (int col =0; col<4; col++)
                 {
-                    TextView tileText = new TextView(this);
+                    MyTextView tileText = new MyTextView(this);
                     GridLayout.Spec rowSpec = GridLayout.InvokeSpec(row);
                     GridLayout.Spec colSpec = GridLayout.InvokeSpec(col);
 
@@ -83,12 +86,17 @@ namespace SlidingPuzzle
                     coordList.Add(tileLocation);
                     tilesList.Add(tileText);
 
+                    //remember the position of the tile
+                    tileText.coordX = tileLocation.X;
+                    tileText.coordY = tileLocation.Y;
+
+                    tileText.Touch += TouchTile;
                     mainLayout.AddView(tileText);
 
                     tileCount = tileCount+1;
                 }
             }
-            mainLayout.RemoveView((TextView)tilesList[15]);
+            mainLayout.RemoveView((MyTextView)tilesList[15]);
             tilesList.RemoveAt(15);
         }
 
@@ -97,7 +105,7 @@ namespace SlidingPuzzle
             Random randomiser = new Random();
             ArrayList copyCoordList = new ArrayList(coordList);
 
-            foreach (TextView tile in tilesList)
+            foreach (MyTextView tile in tilesList)
             {
                 int randIndex = randomiser.Next(0, copyCoordList.Count);
                 Point randomisedLocation = (Point)copyCoordList[randIndex];
@@ -106,6 +114,10 @@ namespace SlidingPuzzle
                 GridLayout.Spec colSpec = GridLayout.InvokeSpec(randomisedLocation.Y);
                 GridLayout.LayoutParams randTileParams = new GridLayout.LayoutParams(rowSpec, colSpec);
 
+                //keep location of tile
+                tile.coordX = randomisedLocation.X;
+                tile.coordY = randomisedLocation.Y;
+                
                 randTileParams.Width = tileWidth - 10;
                 randTileParams.Height = tileWidth - 10;
                 randTileParams.SetMargins(5, 5, 5, 5);
@@ -113,12 +125,53 @@ namespace SlidingPuzzle
                 tile.LayoutParameters = randTileParams;
                 copyCoordList.RemoveAt(randIndex);
             }
+            emptySpot = (Point)copyCoordList[0];
         }
 
         void Reset(object sender, EventArgs e)
         {
             RandomiseTiles();
+        }  
+        void TouchTile(object sender, View.TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Up)
+            {
+                MyTextView tile = (MyTextView)sender;
+
+                Console.WriteLine("tile is at x={0}, y={0}", tile.coordX, tile.coordY);
+
+                //calculate distance between empty spot and the touched tile
+                float xDif = (float)Math.Pow(tile.coordX - emptySpot.X, 2);
+                float yDif = (float)Math.Pow(tile.coordY - emptySpot.Y, 2);
+                float distance = (float)Math.Sqrt(xDif + yDif);
+                //tile next to empty spot
+                if (distance ==1)
+                {
+                    Point currentPoint = new Point(tile.coordX, tile.coordY);
+                    GridLayout.Spec rowSpec = GridLayout.InvokeSpec(emptySpot.X);
+                    GridLayout.Spec colSpec = GridLayout.InvokeSpec(emptySpot.Y);
+                    GridLayout.LayoutParams newLocationParams = new GridLayout.LayoutParams(rowSpec, colSpec);
+                    tile.coordX = emptySpot.X;
+                    tile.coordY = emptySpot.Y;
+
+                    newLocationParams.Width = tileWidth - 10;
+                    newLocationParams.Height = tileWidth - 10;
+                    newLocationParams.SetMargins(5, 5, 5, 5);
+                    tile.LayoutParameters = newLocationParams;
+                    emptySpot = currentPoint;
+                }
+            }
         }
+    }
+    class MyTextView : TextView
+    {
+        Activity myContext;
+        public MyTextView(Activity context):base(context)
+        {
+            myContext = context;
+        }
+        public int coordX { get; set; }
+        public int coordY { get; set; }
     }
 }
 
